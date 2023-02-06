e480ba99ce6a9c1dc46db183565705469a8b4c1a
davidegattini <davidegatto99@gmail.com>
2023-02-06 12:01:04 +0100

    Pulizia script

--- python_script/exts_used_count.py
+++ python_script/exts_used_count.py
@@ -7,10 +7,6 @@ for dir in os.listdir(root):
     dir_path = f"{root}/{dir}"
     for file in os.listdir(dir_path):
         filename, file_ext = os.path.splitext(file)
-        filename = filename.strip()
-        file_ext = file_ext.strip()
-        if file_ext == '':
-            print("Ext empty for file: ", filename)
         if file_ext in exts_used.keys():
             exts_used[file_ext] += 1
         else: 
