054a78a665a01ca2298796e1fd018ce5ffa13b1b
Davide Molitierno <davidmolitiern@hotmail.it>
2023-02-07 15:51:07 +0100

    test con strategia instance based

--- python_script/compute_match.py
+++ python_script/compute_match.py
@@ -3,5 +3,5 @@ from valentine.algorithms import Coma
 from valentine.algorithms import Cupid
 
 def calculate_match(tupla):
-    matcher = Coma(strategy="COMA_OPT")
+    matcher = Coma(strategy="COMA_OPT_INST")
     return tupla[0], tupla[1], valentine_match(tupla[2], tupla[3], matcher)
